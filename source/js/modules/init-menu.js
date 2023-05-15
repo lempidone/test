4130f65470edf17a90e0d7746db792a36438eb24
lempidone <11581480-lempidone@users.noreply.gitlab.com>
2023-05-15 16:54:00 +0300

    Обновляет билд

--- source/js/modules/init-menu.js
+++ source/js/modules/init-menu.js
@@ -1,21 +1,26 @@
 const header = document.querySelector('header');
 const menuToggle = document.querySelector('[data-menu="toggle"]');
 const nav = document.querySelector('[data-main-nav]');
+const links = nav.querySelectorAll('.main-nav__link');
 
 const openMenu = () => {
   header.classList.add('is-menu');
   nav.classList.add('is-active');
   menuToggle.ariaPressed = 'true';
+  window.scrollLock.disableScrolling();
 };
 
 const closeMenu = () => {
   header.classList.remove('is-menu');
   nav.classList.remove('is-active');
   menuToggle.ariaPressed = 'false';
+  window.scrollLock.enableScrolling();
 };
 
 const initMenu = () => {
-  const links = nav.querySelectorAll('.main-nav__link');
+  if (!nav) {
+    return;
+  }
 
   links.forEach((item, index) => {
     item.style.transitionDelay = `${0.33 + index * 0.08}s`;
@@ -32,4 +37,4 @@ const initMenu = () => {
   }
 };
 
-export {initMenu};
+export {initMenu, closeMenu, openMenu};
